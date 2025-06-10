0c66c3cb131eed2ad4de7cd70c1bedda0ab12df5
dd <dyn76614@adobe.com>
2025-06-09 21:11:09 -0400

    Initial Commit

---
+++ infraproxy/infraproxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  infraproxyApp.swift
+//  infraproxy
+//
+//  Created by Doug Downer on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct infraproxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
